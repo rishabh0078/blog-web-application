a42791a2ff394b74b46fbc31e5a0d04e00c7fac5
rishabh <rishabhchaudhary058@gmail.com>
2025-11-03 12:41:36 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://blog-web-application-inky.vercel.app'],
   credentials: true,
 }));
 app.use(express.json());
